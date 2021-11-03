9aecf49585c731d4bd4e0339a9240922f8e69019
Anshu Yaduvanshi <55440206+anshu-byte@users.noreply.github.com>
2021-11-03 15:59:59 +0530

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,81 +1,85 @@
 create table time_table (
-	course_name varchar(50) NOT NULL,
-	courseID varchar(5) NOT NULL,
-    day_ varhar(3) not null,
-	timing  varchar(30) NOT NULL,
+	course_name varchar NOT NULL,
+	courseID varchar NOT NULL,
+    day_ varhar not null,
+	timing  varchar NOT NULL,
 	primary key(timing)
 );
 
 CREATE TABLE course_catalogue(
-courseID varchar(5) NOT NULL, 
-course_name varchar(30) NOT NULL,
-department_name varchar(6) NOT NULL,
+courseID varchar NOT NULL, 
+course_name varchar NOT NULL,
+department_name varchar NOT NULL,
 credit integer not null,
 primary key(courseID)
 );
 
 
 create table pre_requisite_courses(
-    course_id varchar(5) not null,
-    course_name varchar(30) NOT NULL,
-    prerequisite_course_id varchar(5) not null,
-    primary key(course_id,prerequisite_course_id)
+    course_id varchar not null,
+    course_name varchar NOT NULL,
+    prerequisite_course_id varchar not null,
+    primary key(pre_requisite_course_id)
 );
 
 create table eligible_departments(
-    course_id varchar(5) not null,
-    course_name varchar(30) NOT NULL,
-    depart_name varchar(30) not null,
-    primary key(course_id,depart_name)
+    course_id varchar not null,
+    course_name varchar NOT NULL,
+    depart_name varchar not null,
+    primary key(depart_name)
 );
 
 
 create table courses_offered(
-courseId varchar(5) not null, 
+courseId varchar not null, 
 year_ integer not null, 
-course_name varchar(30) not NULL, 
-sem varchar(30) not null, 
+course_name varchar not NULL, 
+sem varchar not null, 
 cgpa_required Dec(4,2) not NULL, 
-instructor_id varchar(30) not NULL, 
-timing varchar(30) not NULL, 
-depart_name varchar(30) not NULL,
+instructor_id varchar not NULL, 
+timing varchar not NULL, 
+depart_name varchar not NULL,
 primary key(courseid, instructor_name));
    
 create table time_table (
-	course_name varchar(50) NOT NULL,
-	courseID varchar(5) NOT NULL,
-    day_ varhar(3) not null,
-	timing  varchar(30) NOT NULL,
+	course_name varchar NOT NULL,
+	courseID varchar NOT NULL,
+    day_ varhar not null,
+	timing  varchar NOT NULL,
 	primary key(timing)
 );
 
 create table section(
-    section_name varchar(2) not null,
-    course_id varchar(30) not null,
-    course_name varchar(30) not null,
-    instructor_id varchar(30) not null,
+    section_name varchar not null,
+    course_id varchar not null,
+    course_name varchar not null,
+    instructor_id varchar not null,
     primary key(section_name)
 );
 
 create table takes(
-    student_id varchar(30) not null,
-    student_name varchar(30) not null,
-    course_id varchar(30) not null,
+    student_id varchar not null,
+    student_name varchar not null,
+    course_id varchar not null,
     grade_on_course integer not null,
 	credit_of_course integer not null,
-    depart_name varchar(10) not null,
+    depart_name varchar not null,
     year_ integer not null,
-    timing varchar(30) NOT NULL,
+    timing varchar NOT NULL,
     sem integer not null,
     status integer not null,
     primary key(student_id)
 );
 
 create table ticket(
-    student_id varchar(30) not null,
-    course_id varchar(30) not null,
-    instructor_id varchar(30) not null,
-    course_name varchar(30) not null,
+    student_id varchar not null,
+    student_name varchar not null,
+    sem integer not null,
+    year_ integer not null,
+    depart_name varchar not null,
+    course_id varchar not null,
+    instructor_id varchar not null,
+    course_name varchar not null,
     status_faculty integer not null,
     status_batch_advisor integer not null,
     status_dean_academics integer not null,
@@ -83,21 +87,20 @@ create table ticket(
 );
 
 create table instructor (
-  instructor_id varchar(30) not null,
-  name_ varchar(50) not null,
-  department_name varchar(30) ,
+  instructor_id varchar not null,
+  name_ varchar not null,
+  department_name varchar ,
   PRIMARY KEY (instructor_id)
 );
 
 create table batch_advisor(
-    instructor_id varchar(30) not null,
-    nam_ varchar(30) not null,
-    department_name varchar(30) not null,
+    instructor_id varchar not null,
+    nam_ varchar not null,
+    department_name varchar not null,
     primary key (instructor_id)
 )
 
 create table Dean (
-    dean_id varchar(30) not null,
-    nam_ varchar(30) not null,    
+    dean_id varchar not null,  
     primary key (dean_id) 
 )
